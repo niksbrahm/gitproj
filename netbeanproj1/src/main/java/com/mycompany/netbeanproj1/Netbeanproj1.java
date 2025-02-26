e5a58c22c8f2cb59629c2eaa3df31d7f15ad5740
niksbrahm <bshainiweb@gmail.com>
2025-02-25 20:53:10 -0600

    initial project

---
+++ netbeanproj1/src/main/java/com/mycompany/netbeanproj1/Netbeanproj1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.netbeanproj1;
+
+/**
+ *
+ * @author niks_
+ */
+public class Netbeanproj1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
